712f1ed7f4347a02780183d2a84d58aedc173f83
dakuritsu <Pierre.eknb@gmail.com>
2024-06-15 14:36:40 +0200

    test

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+  echo "pseudo = 'admin'";
+  echo "<br>";
+  echo "mot de passe = 'admin'";
+
+
+  echo "pseudo = 'daku'";
+  echo "<br>";
+  echo "mot de passe = '123123'";
+
+  echo "pseudo = 'jesuisutilisateur'";
+  echo "<br>";
+  echo "mot de passe = 'utilisateur'";
+?>
